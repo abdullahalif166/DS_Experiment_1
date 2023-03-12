1de77e5408475cd963bbb02942596e10edc7dd9b
abdullahalif168 <abdullah.alif.168@gmail.com>
2023-03-12 15:35:45 +0600

    first commit

---
+++ Client4.java
@@ -0,0 +1,39 @@
+package ManyToMany;
+
+import java.net.Socket;
+
+import java.util.Scanner;
+
+import java.io.PrintWriter;
+
+
+public class Client4 {
+    public static void main(String[] args) {
+        String name = "empty";
+        String reply = "empty";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name (Please enter your name to join the chat): ");
+        reply = sc.nextLine();
+        name = reply;
+
+        try (Socket socket = new Socket("localhost", 5000)) {
+            PrintWriter cout = new PrintWriter(socket.getOutputStream(), true);
+
+            ThreadClient threadClient = new ThreadClient(socket);
+            new Thread(threadClient).start(); // start thread to receive message
+
+            cout.println(reply + ": has joined chat-room.");
+            do {
+                String message = (name + " : ");
+                reply = sc.nextLine();
+                if (reply.equals("logout")) {
+                    cout.println("logout");
+                    break;
+                }
+                cout.println(message + reply);
+            } while (!reply.equals("logout"));
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
